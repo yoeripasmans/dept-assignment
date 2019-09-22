4005bfcbeb637150f72f335e7dfbdab6f5e5d736
Yoeri Pasmans <yoerip@live.nl>
2019-09-22 15:24:54 +0200

    Add introduction hero component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import GlobalStyle from 'styles';
 
+import MainHeader from 'components/modules/MainHeader';
+import IntroductionHero from 'components/modules/IntroductionHero';
+
 const App = () => {
   return (
     <>
       <GlobalStyle />
-      <h1>It works</h1>
+      <MainHeader />
+      <IntroductionHero />
     </>
   );
 };
---
+++ src/components/modules/IntroductionHero/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import headerImage from 'static/images/florensis.png';
+
+const IntroductionHero = () => {
+  return (
+    <header>
+      <img src={headerImage} alt="Header" />
+    </header>
+  );
+};
+
+export default IntroductionHero;
