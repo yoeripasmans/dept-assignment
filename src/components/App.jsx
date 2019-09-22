f79fc45f38eedb79474fe3b1471ba693da5262f9
Yoeri Pasmans <yoerip@live.nl>
2019-09-22 14:05:48 +0200

    Initialize project

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import GlobalStyle from 'styles';
+
+const App = () => {
+  return (
+    <>
+      <GlobalStyle />
+      <h1>It works</h1>
+    </>
+  );
+};
+
+export default App;
---
+++ src/styles/index.js
@@ -0,0 +1,41 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css?family=Open+Sans:400,600,700|Poppins:400,600,700');
+
+  html {
+    box-sizing: border-box;
+  }
+
+  *,
+  *:after,
+  *:before {
+    box-sizing: inherit;
+  }
+
+  body {
+      font-family: 'Open Sans', sans-serif;
+      padding: 0;
+      margin: 0;
+      -webkit-font-smoothing: antialiased;
+  }
+
+  main {
+      max-width: 77rem;
+      margin: 0 auto;
+      padding: 0 1rem;
+  }
+
+  h1 {
+      font-size: 2.5rem;
+  }
+
+  h3 {
+      font-size: 1.7rem;
+      font-weight: 600;
+  }
+
+  input[type=search] {
+      -webkit-appearance: none;
+  }
+`;
